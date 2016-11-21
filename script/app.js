9920aa9715858298017fe985d3b06fa2b99b7f68
Frederico Silva <frederico.x.silva@accenture.com>
2016-11-21 20:41:34 -0200

    Form vaidations

--- script/app.js
+++ script/app.js
@@ -104,4 +104,13 @@
         };
     });
     
+    app.controller('ReviewController', function(){
+        this.review = {};
+        this.addReview = function(product){
+            this.review.createdOn = Date.now();
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+    
 })();
