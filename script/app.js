1f401d04f54671c1adc1c52446cdd33fd561e94a
Frederico Silva <fredericochopin@gmail.com>
2016-11-22 09:45:02 -0200

    Directives

--- script/app.js
+++ script/app.js
@@ -113,4 +113,46 @@
         };
     });
     
+    //dash in html translates to camelCase in JavaScript
+    app.directive('productDescription', function(){
+        //it expects returning a configuration file
+        return {
+            restrict: 'E', //Type of directive (E for element)
+            templateUrl: 'product-description.html' //URL of the template
+            
+        };
+    });
+    
+    app.directive("productSpecs", function(){
+        return {
+            restrict: "A",
+            templateUrl: "product-specs.html"
+        };
+    });
+    
+    app.directive("productReviews", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-reviews.html"
+        };
+    });
+    
+    app.directive("productTabs", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-tabs.html",
+            controller: "TabController as tab"
+        };
+    });
+    
+    app.directive("productGallery", function() {
+        return {
+            restrict: 'E',
+            templateUrl: "product-gallery.html",
+            controller: "GalleryController as gallery"
+        };
+    });
+    
+    
+    
 })();
